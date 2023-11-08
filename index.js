ca9233982f210c0a8a4e38366d2308c14a501cd5
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-11-08 23:16:39 +0600

    Protect routes from the unauthorized user

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://job-hunter-188ab.web.app", "https://job-hunter-188ab.firebaseapp.com"],
     credentials: true,
   })
 );
@@ -50,7 +50,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //Jobs Collections Name
     const JobCategoryCollection = client.db("JobHunterDB").collection("JobCategory");
@@ -65,17 +65,16 @@ async function run() {
 
       res
         .cookie("token", token, {
-          httpOnly: true,
-          secure: false,
-          // maxAge: 0,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
 
-    app.post('/logout', async(req, res)=>{
+    app.post("/logout", async (req, res) => {
       const user = req.body;
-      res.clearCookie('token',{maxAge: 0}).send({success: true})
-    })
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
 
     //Get Job Category Info
     app.get("/api/v1/jobsCategory", async (req, res) => {
@@ -100,14 +99,21 @@ async function run() {
 
     //get specifice job info
     app.get("/api/v1/jobs/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await JobsInfoCollection.findOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await JobsInfoCollection.findOne(query);
+        res.send(result);
+      } catch (error) {
+        res.send({ message: error.message });
+      }
     });
 
     //get specifice a user posted job
-    app.get("/api/v1/my/jobs", async (req, res) => {
+    app.get("/api/v1/my/jobs", verifyToken, async (req, res) => {
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "Forbiden Access" });
+      }
       let query = {};
       if (req.query?.userEmail) {
         query = { userEmail: req.query.userEmail };
@@ -179,8 +185,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
